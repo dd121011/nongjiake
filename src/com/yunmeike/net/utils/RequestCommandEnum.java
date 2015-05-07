b213e95b94f9c594b5473d5ac24558fc8dd6feb5
ljbgithub <353782796@qq.com>
2015-05-07 18:03:11 +0800

    修改部分资源

---
+++ src/com/yunmeike/net/utils/RequestCommandEnum.java
@@ -0,0 +1,22 @@
+package com.yunmeike.net.utils;
+
+public enum RequestCommandEnum {
+	FAMILY_LIST("/api/family/familylist"),
+	SEND_CODE_DO("/api/register/send_code_do"),
+	CHECK_MOBILE_DO("/api/register/check_mobile_do"),
+	LOGIN("/api/account/login"),
+	APPINFOS_AREAS("/api/appinfos/areas"),
+	FAMILY_GETSCENIC("/api/family/getscenic"),
+	FAMILY_INFO("/api/family/family_info"),
+	FINDFAMILY_LISTLBS("/api/family/findfamilyListlbs"),
+	FAMILY_FAV_DO("/api/family/fav_do"),
+	FAMILY_CANCEL_FAV("/api/family/cancel_fav"),
+	FAMILY_REVIEW_DO("/api/family/review_do");
+
+	public String command;
+	RequestCommandEnum(String name) {
+		this.command = name;
+	}
+	
+
+}
